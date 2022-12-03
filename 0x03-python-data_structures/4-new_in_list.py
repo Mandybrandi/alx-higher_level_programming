57adf4c841b5c142610da7359e4928c37481bd71
mandybrandi <williamsvictor2000@gmail.com>
2022-12-03 13:48:40 -0800

    data structure

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    length = len(my_list)
+new_in_list = __import__('4-new_in_list').new_in_list
 
-    new_list = my_list[:]
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_element = 9
+new_list = new_in_list(my_list, idx, new_element)
 
-    if 0 <= idx < length:
-        new_list[idx] = element
-
-        return (new_list)
+print(new_list)
+print(my_list)
