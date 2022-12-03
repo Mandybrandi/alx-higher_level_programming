65e805d4b13494337c55c588ae832b6c8ee0aaa7
mandybrandi <williamsvictor2000@gmail.com>
2022-12-03 13:51:13 -0800

    data structure

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-new_in_list = __import__('4-new_in_list').new_in_list
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)
+def new_in_list(my_list, idx, element):
+    ''' Write a function that replaces an element in a list at a
+    specific position without modifying the original list (like in C).'''
+    if idx < 0:
+        return (my_list)
+    elif idx > (len(my_list) - 1):
+        return (my_list)
+    else:
+        if my_list:
+            new_list = my_list.copy()
+            new_list[idx] = element
+            return (new_list)
+        return (my_list)
